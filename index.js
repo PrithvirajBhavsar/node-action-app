180b7db744892d4898392ccec4805eb2ed73898f
Prithviraj Bhavsar <prithvirajbhavsar@gmail.com>
2026-09-19 10:27:20 +0530

    start

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const PORT = 3000;
 
 // Define a basic route for the root URL
 app.get('/', (req, res) => {
-    res.send('Hello World from Express! Prithviraj Bhavsar, mjaa aara h');
+    res.send(`Hello World from Express!Let's create apis now`);
 });
 
 // Start the server
